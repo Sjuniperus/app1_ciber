259b6cf2dfd33e8515b35516a74705f417598a94
Sjuniperus <sapindacea@gmail.com>
2023-09-13 19:00:24 +0200

    validation y sanitation form

--- backend/controller/FormController.js
+++ backend/controller/FormController.js
@@ -3,16 +3,43 @@ import { check, validationResult } from 'express-validator';
 
 
 //Crud (crear, leer, actulizar, borrar)
-
+ 
 //crear
+const xss = require('xss');
+const filter = new xss.Filter();
 
 export const createForm = async (req, res) =>{
-    const {Email, Name, Subject, Message} = req.body
-    if (!Email || !Name || !Subject || !Message ){
-        return res.status(400).json ({message: "Todos los campos son requeridos."})
+
+    const { Email, Name, Subject, Message } = req.body;
+    
+
+    //validación: bail() frena validación si no se cumple alguna regla; trim(), quita espacios en blanco; escape(), convierte caracteres HTML y JS,(<, >, y ") en entidades de HTML -->texto plano [XSS] 
+    const validationRules = [
+        check('Email').isEmail().withMessage('El campo Email debe ser una dirección de correo válida').bail().trim().escape(),
+        check('Name').isAlpha('es-ES', { ignore: ' ' }).withMessage('El campo Name no válido').bail().trim().escape(),
+        check('Subject').matches(/^[a-zA-Z0-9\s\-]+$/).withMessage('El campo Subject no válido').bail().trim().escape(),
+        check('Message').matches(/^[a-zA-Z0-9\s\-]+$/).withMessage('El campo message no válido').bail().trim().escape(),
+    ];
+    //espera a que todas las validaciones se ejecuten
+    await Promise.all(validationRules.map(validation => validation.run(req)));
+    
+    // Sanitizar
+    const sanitizedEmail = filter.process(Email);
+    const sanitizedName = filter.process(Name);
+    const sanitizedSubject = filter.process(Subject);
+    const sanitizedMessage = filter.process(Message);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const form = await FormLock.create(req.body)
+        const form = await FormLock.create({
+            Email: sanitizedEmail,
+            Name: sanitizedName,
+            Subject: sanitizedSubject,
+            Message: sanitizedMessage})
+        
         res.status(201).json(form)
         
     } catch (error) {
